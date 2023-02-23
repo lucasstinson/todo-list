b2d9f2f6f2ca4e65b2007b0e4d2cc39740d9debe
Luke <user@pop-os.localdomain>
2023-02-23 17:43:08 -0600

    Updated file path

--- src/modules/sideBar.js
+++ src/modules/sideBar.js
@@ -55,7 +55,7 @@ const createSideBar = () => {
 
   const allTodoImage = document.createElement("img");
   allTodoImage.classList = "all-todo-image";
-  allTodoImage.src = "../dist/images/inbox.png";
+  allTodoImage.src = "./images/inbox.png";
   allTodoWrapper.appendChild(allTodoImage);
 
   const allTodos = document.createElement("div");
